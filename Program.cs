f1c00812c4c9dcceb4106b8f9d152bad9a5b135c
João Pedro <166267171+bjotaPe@users.noreply.github.com>
2025-09-11 03:52:10 +0000

    feat: add functions

--- Program.cs
+++ Program.cs
@@ -16,3 +16,22 @@ Console.WriteLine($"Volume após mute: {tv.Volume}");
 tv.AlterarMudo();
 Console.WriteLine($"Volume após desmute: {tv.Volume}");
 
+tv.ProximoCanal();
+Console.WriteLine($"Canal após aumentar: {tv.Canal}");
+
+tv.CanalAnterior();
+Console.WriteLine($"Canal após diminuir: {tv.Canal}");
+
+tv.MudarCanal(100);
+Console.WriteLine($"Mudar de canal direto: {tv.Canal}");
+
+tv.MudarCanal(600);
+Console.WriteLine($"Canal 600 inválido! Mantido no canal {tv.Canal}");
+
+tv.MudarCanal(50);
+tv.Desligar();
+Console.WriteLine($"TV está desligada? {tv.Estado}");
+
+tv.MudarCanal(200);
+tv.Ligar();
+Console.WriteLine($"Canal após ligar: {tv.Canal}");
--- Televisao.cs
+++ Televisao.cs
@@ -3,9 +3,17 @@ public class Televisao
     private const int VOL_MAX = 100;
     private const int VOL_MIN = 0;
     private int VolumeAnterior;
+
+    private const int CANAL_MIN = 1;
+    private const int CANAL_MAX = 520;
+    private int UltimoCanal;
+
     public Televisao(float tamanho)
     {
         Tamanho = tamanho;
+        Canal = CANAL_MIN;
+        UltimoCanal = Canal;
+        Volume = 10;
     }
 
     public float Tamanho { get ;}
@@ -41,6 +49,42 @@ public class Televisao
             VolumeAnterior = Volume;
             Volume = 0;
         }
+    }
+
+    public void ProximoCanal() 
+    {
+        if(Canal < CANAL_MAX)
+            Canal++;    
+        else
+            Console.WriteLine("Já está no último canal");
+    } 
+
+    public void CanalAnterior() 
+    {
+        if(Canal > CANAL_MIN)
+            Canal--;    
+        else
+            Console.WriteLine("Já está no primeiro canal");
+    }
+
+    public void MudarCanal(int numero)
+    {
+        if(numero >= CANAL_MIN && numero <= CANAL_MAX)
+            Canal = numero;
+        else
+            Console.WriteLine("Canal inválido");
+    }
+
+    public void Ligar()
+    {
+        Canal = UltimoCanal; // quando liga, volta para o último canal
+        Estado = true;
+    }
+
+    public void Desligar()
+    {
+        UltimoCanal = Canal; // quando desliga, guarda o canal atual
+        Estado = false;
     }
 
 }
